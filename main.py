3d9e84d7611c5cf55e25437285d0028f261f03b3
22Vlad222 <osvlad2010@gmail.com>
2026-01-31 16:20:42 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
 Window.size = (300, 500)
-Window.clearcolor=(0, 0.5, 0, 1)
+Window.clearcolor = (0, 0.5, 0, 1)
 
 class Delivery(Widget):
     pass
